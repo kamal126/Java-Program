1b03c76fa4a3db8dcce7417f20ccd1d29941c662
Kamal Chandra <54989946+kamal126@users.noreply.github.com>
2023-02-27 14:27:30 +0530

    Update Prime

--- Basic/Prime.java
+++ Basic/Prime.java
@@ -20,3 +20,32 @@ public class Prime {
         }//end of else
     }
 }
+
+/**
+ * Prime Number Program using Method in Java
+ */
+class InnerPrime {
+    
+    static void checkPrime(int n){
+        int i,m=0,flag=0;
+        m=n/2;
+        if (n==0||n==1) {
+            System.out.println(n+" is not prime number");
+        } else {
+            for(i=2;i<=m;i++){
+                if(n%i==0){
+                    flag=1;
+                    break;
+                }
+            }
+            if (flag==0) {
+                System.out.println(n+" is prime number");
+            }
+        }//end of else
+    }
+    public static void main(String[] args) {
+        for (int i = 0; i <=10; i++) {
+            checkPrime(i);
+        }
+    }
+}
